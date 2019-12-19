ead6f858bd33d1927b8b41e32dbc65b5bf8fd424
daniel mcnamara <danmac8407@gmail.com>
2019-12-19 09:46:39 -0600

    answered more questions

--- index.js
+++ index.js
@@ -188,8 +188,13 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const runnerNames = []
+  runners.forEach(item => {
+    runnerNames.push(`${item.last_name}, ${item.first_name}`)
+  })
+
+  return runnerNames
 }
 
 /**
@@ -204,8 +209,8 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+
 }
 
 /**
